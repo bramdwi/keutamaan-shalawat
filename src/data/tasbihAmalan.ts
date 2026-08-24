24c67493f3846f9b2b7d6fb8cbdc038d66ced163
Brahman Dwi Budiartomo <bramdwi@Brahmans-MacBook-Air.local>
2026-08-24 23:48:18 +0700

    fix: koreksi teliti seluruh rujukan pasal, nomor hadits, dan tautan bab pada tasbih digital

--- src/data/tasbihAmalan.ts
+++ src/data/tasbihAmalan.ts
@@ -57,9 +57,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā Muḥammadin wa āli Muḥammad',
     translation: 'Ya Allah, limpahkanlah sholawat kepada Nabi Muhammad dan keluarga Nabi Muhammad.',
     fadhilah: 'Barangsiapa bersholawat 1.000 kali pada hari Jumat, ia tidak akan meninggal dunia melainkan telah diperlihatkan tempat duduknya di dalam surga.',
-    source: 'Bab II: Pasal 3 (Hadits 3)',
+    source: 'Bab II: Pasal 4 (Hadits 3)',
     targetChapterId: 'bab-2',
-    targetSectionId: 'bab2-sec-3'
+    targetSectionId: 'bab2-sec-4'
   },
   {
     id: 'wirid-jumat-7-pekan',
@@ -72,9 +72,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā Muḥammadin wa anzilhul-maq\'adal-muqarraba \'indaka yaumal-qiyāmah',
     translation: 'Ya Allah, limpahkanlah sholawat kepada Nabi Muhammad dan tempatkanlah beliau pada kedudukan yang amat dekat di sisi-Mu pada hari kiamat.',
     fadhilah: 'Dibaca 7 kali setiap hari Jumat selama 7 pekan berturut-turut untuk meraih kepastian syafaat Rasulullah ﷺ dan kedudukan mulia di Maqam Al-Muqarrab.',
-    source: 'Bab II: Pasal 7 (Hadits 6)',
+    source: 'Bab II: Pasal 6 (Hadits 5)',
     targetChapterId: 'bab-2',
-    targetSectionId: 'bab2-sec-7'
+    targetSectionId: 'bab2-sec-6'
   },
   {
     id: 'wirid-ruyah-nabi',
@@ -119,9 +119,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Ṣallallāhu \'alā Muḥammad',
     translation: 'Semoga Allah melimpahkan sholawat (rahmat) kepada Nabi Muhammad ﷺ.',
     fadhilah: 'Barangsiapa bersholawat 100 kali setiap hari, niscaya Allah mengabulkan 100 hajatnya (30 hajat dunia dan 70 hajat akhirat) serta mendapatkan salam langsung dari Baginda Rasulullah ﷺ.',
-    source: 'Bab I: Pasal 7 (Hadits 3)',
+    source: 'Bab I: Pasal 11',
     targetChapterId: 'bab-1',
-    targetSectionId: 'bab1-sec-7'
+    targetSectionId: 'bab1-sec-11'
   },
   {
     id: 'wirid-harian-300',
@@ -134,7 +134,7 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā sayyidinā Muḥammadin \'abdika wa rasūlikan-nabiyyil-ummiyy',
     translation: 'Ya Allah, limpahkanlah sholawat kepada junjungan kami Nabi Muhammad, hamba-Mu dan Rasul-Mu, Nabi yang ummi.',
     fadhilah: 'Batas minimal harian menurut Kitab Qutul Qulub dan bimbingan Syaikh Rasyid Ahmad Gangohi agar seorang hamba terhindar dari predikat kikir dan tidak tahu balas budi atas jasa agung Rasulullah ﷺ.',
-    source: 'Bab III: Pasal 4',
+    source: 'Bab III: Pasal 4 (Hadits 3)',
     targetChapterId: 'bab-3',
     targetSectionId: 'bab3-sec-4'
   },
@@ -196,9 +196,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā Muḥammadin-nabiyyi wa azwājihī ummahātil-mu\'minīn, wa dzurriyyatihī wa ahli baitihī kamā ṣallaita \'alā āli Ibrāhīma innaka Ḥamīdum Majīd',
     translation: 'Ya Allah, limpahkanlah sholawat kepada Nabi Muhammad, para istrinya ibunda kaum mukminin, keturunannya, dan keluarganya, sebagaimana Engkau telah melimpahkan sholawat kepada keluarga Nabi Ibrahim, sesungguhnya Engkau Maha Terpuji lagi Maha Mulia.',
     fadhilah: 'Rasulullah ﷺ bersabda: "Barangsiapa senang ditakar pahalanya dengan takaran yang paling penuh dan paling sempurna saat bersholawat kepada kami, hendaklah ia membaca sholawat ini."',
-    source: 'Bab II: Pasal 2 (Hadits 2)',
+    source: 'Bab II: Pasal 3 (Hadits 2)',
     targetChapterId: 'bab-2',
-    targetSectionId: 'bab2-sec-2'
+    targetSectionId: 'bab2-sec-3'
   },
   {
     id: 'wirid-wasilah-adzan',
@@ -258,9 +258,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā sayyidinā Muḥammadin ṣalātan tunjīnā bihā min jamī\'il-ahwāli wal-āfāt, wa taqḍī lanā bihā jamī\'al-ḥājāt, wa tuṭahhirunā bihā min jamī\'is-sayyi\'āt, wa tarfa\'unā bihā \'indaka a\'lad-darajāt, wa tuballighunā bihā aqṣal-ghāyāti min jamī\'il-khairāti fil-ḥayāti wa ba\'dal-mamāt',
     translation: 'Ya Allah, limpahkanlah sholawat kepada junjungan kami Nabi Muhammad, dengan sholawat yang menyelamatkan kami dari segala bencana dan marabahaya, mengabulkan seluruh hajat kami, menyucikan kami dari segala keburukan, mengangkat kami ke derajat tertinggi di sisi-Mu, dan menyampaikan kami ke puncak cita-cita kebaikan dalam hidup dan sesudah mati.',
     fadhilah: 'Amalan yang diajarkan Rasulullah ﷺ di dalam mimpi kepada Syaikh Musa Adh-Dharir tatkala kapalnya diterjang badai dahsyat di laut lepas, sehingga seluruh penumpang selamat berkat pertolongan Allah.',
-    source: 'Bab V: Kisah 2',
+    source: 'Bab V: Kisah 4',
     targetChapterId: 'bab-5',
-    targetSectionId: 'bab5-sec-2'
+    targetSectionId: 'bab5-sec-4'
   },
   {
     id: 'wirid-khamsah-syafii',
@@ -273,9 +273,9 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā Muḥammadin kullamā dzakarahudz-dzākirūn, wa ghafala \'an dzikrihil-ghāfilūn',
     translation: 'Ya Allah, limpahkanlah sholawat kepada Nabi Muhammad setiap kali orang-orang yang berdzikir mengingat beliau, dan setiap kali orang-orang yang lalai melupakan beliau.',
     fadhilah: 'Sebab Allah mengampuni dosa Imam Asy-Syafi\'i dan mengangkat derajatnya ke maqam tertinggi di surga tanpa hisab.',
-    source: 'Bab V: Kisah 6',
+    source: 'Bab V: Kisah 3',
     targetChapterId: 'bab-5',
-    targetSectionId: 'bab5-sec-6'
+    targetSectionId: 'bab5-sec-3'
   },
   {
     id: 'wirid-ubay-kaab',
@@ -288,8 +288,8 @@ export const tasbihAmalanList: TasbihAmalan[] = [
     latin: 'Allāhumma ṣalli \'alā sayyidinā Muḥammadin wa \'alā āli sayyidinā Muḥammad',
     translation: 'Ya Allah, limpahkanlah sholawat kepada junjungan kami Nabi Muhammad dan kepada keluarga junjungan kami Nabi Muhammad.',
     fadhilah: 'Tatkala Sayyidina Ubay bin Ka\'ab radhiyallahu \'anhu berkata: "Aku akan menjadikan seluruh waktu doaku khusus untuk bersholawat kepadamu", Rasulullah ﷺ bersabda: "Jika demikian, Allah akan mencukupi segala urusan kegelisahan duniamu dan mengampuni seluruh dosamu."',
-    source: 'Bab I: Pasal 5 (Hadits 1)',
+    source: 'Bab I: Pasal 18 (Hadits 9)',
     targetChapterId: 'bab-1',
-    targetSectionId: 'bab1-sec-5'
+    targetSectionId: 'bab1-sec-18'
   }
 ];
